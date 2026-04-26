cde1e620af4a7bc2c56b77bcd9eb8a46f0fd693d
astralsc <greedycellhelp@gmail.com>
2026-04-27 00:03:57 +0800

    remove useless

--- api/v9/auth/login.php
+++ api/v9/auth/login.php
@@ -4,18 +4,18 @@ header('Content-Type: application/json');
 
 $data = [
     "backup" => "True",
-    "login_instance_id" => "530b03b7-bfe2-4bab-998f-40ddfa659036",
+    "login_instance_id" => "test",
     "mfa" => "True",
     "sms" => "False",
-    "ticket" => "OTA5MjgxNTY1ODk5NjQ1MDM4.HQnXy6.fl91KokTMFpuuX7pxiSmQ5D4dYSnDvJ8aNIoH4",
+    "ticket" => "test",
     "totp" => "True",
-    "user_id" => "909281565899645038",
+    "user_id" => "1",
     "webauthn" => "(null)"
 ];
 
 /*$data = [
-    "login" => "MTQ5NzUzNTcyOTIyNjQ4NTkxNA.G6za0t.vF31uzw8mHfEQahHPHCgbTugNJVt5ioVscfEFQ",
-    "user_id" => "1497535729226485914",
+    "login" => "test",
+    "user_id" => "1",
     "user_settings" => [
         "locale" => "en-US",
         "theme" => "dark"
