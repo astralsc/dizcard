00c49c475d0599c4c5b8b66d896a9b70872645f3
Astral <132731805+astralsc@users.noreply.github.com>
2026-04-27 16:16:24 +0800

    official default discord server region

--- api/v9/voice/regions.php
+++ api/v9/voice/regions.php
@@ -4,29 +4,8 @@ header('Content-Type: application/json');
 
 $data = [
     [
-        'id'         => '2016',
-        'name'       => '2015-2016',
-        'optimal'    => false,
-        'deprecated' => false,
-        'custom'     => true,
-    ],
-    [
-        'id'         => '2017',
-        'name'       => '2015-2017',
-        'optimal'    => false,
-        'deprecated' => false,
-        'custom'     => true,
-    ],
-    [
-        'id'         => '2018',
-        'name'       => '2015-2018',
-        'optimal'    => false,
-        'deprecated' => false,
-        'custom'     => true,
-    ],
-    [
-        'id'         => 'everything',
-        'name'       => 'Everything',
+        'id'         => 'rotterdam',
+        'name'       => 'rotterdam',
         'optimal'    => false,
         'deprecated' => false,
         'custom'     => true,
